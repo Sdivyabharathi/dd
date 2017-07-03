27f043f00b3afdd757fdf1c6e5b7657c544ba71c
Sdivyabharathi <divyaarul96@gmail.com>
2017-07-03 21:08:50 +0530

    Create Alphabetcheck.java

---
+++ Alphabetcheck.java
@@ -0,0 +1,20 @@
+# dd
+import java.io.*;
+import java.util.*;
+public class Alphabetcheck
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char ch1;
+ch1=sc.next().charAt(0);
+if((ch1>='a')&&(ch1>='z')||(ch1>='A')&&(ch1>='Z'))
+{
+System.out.println("alphabet");
+}
+else
+{
+System.out.println("not");
+}
+}
+}
