0a1773fb43e4834e67e02112f27078aedadc3d0a
Sdivyabharathi <divyaarul96@gmail.com>
2017-07-04 21:29:58 +0530

    Create One.java

---
+++ One.java
@@ -0,0 +1,17 @@
+# dd
+import java.io.*;
+import java.util.*;
+public class One
+{
+public static void main(String args[])
+{
+int n,fact=1;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(int i=1;i<=5;i++)
+{
+fact=fact*i;
+}
+System.out.println(fact);
+}
+}
